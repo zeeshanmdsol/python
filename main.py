dc07d81388fbf8bb3a2b234d919eaeb22812fcdd
Zeeshi <zeeshanmdsol@gmail.com>
2023-08-16 22:44:32 +0500

    Random walk

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import random
 
 tim = t.Turtle()
 
-########### Draw Shapes In Random Colors ########
-
+########### Random Walk ########
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-for shape_side_n in range(3, 10):
+for _ in range(200):
     tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
