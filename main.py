3160b2b655ff091041b1920763112f1355ee67a5
Zeeshi <zeeshanmdsol@gmail.com>
2023-08-17 13:05:28 +0500

    crossing game

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-import turtle as t
-import random
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-tim = t.Turtle()
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
-########### Draw Shapes In Random Colors ########
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
 
-for shape_side_n in range(3, 10):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
